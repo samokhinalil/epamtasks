0778b26c03e1da3cd789f41004648da845eaf13d
samokhinalil <samokhinalil@yandex.ru>
2019-03-31 15:19:11 +0300

    add task08

---
+++ Samokhina_Liliya_Task08/Task1/DynamicArray.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class DynamicArray<T> where T: new()
+    {
+        private T[] array;
+        public int Length { get; private set; }
+        public int Capacity { get; private set; }
+
+        public DynamicArray()
+        {
+            Capacity = 8;
+            array = new T[Capacity];
+        }
+
+        public DynamicArray(int n)
+        {
+            if (n > 0)
+            {
+                Capacity = n;
+                array = new T[n];
+            }
+            else
+            {
+                throw new Exception("Incorrect array length!");
+            }
+        }
+
+        public DynamicArray(T[] array)
+        {
+            this.array = array;
+            Capacity = array.Length;
+        }
+
+        public void Add(T item)
+        {
+            Insert(Length, item);
+        }
+
+        public void AddRange(T[] addingArray)
+        {
+            var count = addingArray.Length;
+
+            if(Capacity < Length + count)
+            {
+                Capacity = Length + count;
+                Array.Resize(ref array, Capacity);
+            }
+
+            foreach (var item in addingArray)
+            {
+                array[Length++] = item;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            int index = Array.IndexOf(array, item);
+            if (index != -1)
+            {
+                Length--;
+                Array.Copy(array, index + 1, array, index, Length - index);
+                return true;
+            }
+            return false;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index >= 0 && index <= Length)
+            {
+                if (Length == Capacity)
+                {
+                    Capacity *= 2;
+                    Array.Resize(ref array, Capacity);
+                }
+                Array.Copy(array, index, array, index + 1, Length - index);
+                array[index] = item;
+                Length++;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Index out of array range");
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if(index >=0 && index <= Length)
+                {
+                    return array[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Index out of array range");
+                }
+            }
+
+            set
+            {
+                if (index >= 0 && index <= Length)
+                {
+                    array[index] = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Index out of array range");
+                }
+            }
+        }
+    }
+}
---
+++ Samokhina_Liliya_Task08/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray<int> dynamic = new DynamicArray<int>(1);
+            for (int i = 1; i < 5; i++)
+            {
+                dynamic.Add(i);
+            }
+
+            dynamic.AddRange(new int[] { 6, 7, 8 });
+            dynamic.Remove(6);
+            dynamic.Insert(4, 10);
+            Console.WriteLine($"Length = {dynamic.Length}, Capacity = {dynamic.Capacity}");
+            Console.ReadKey();
+        }
+    }
+}
